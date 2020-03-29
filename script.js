f668619b0e2d555f5df896966b9657d68b22854a
Alex Travick <alextravick18@gmail.com>
2020-03-28 20:41:43 -0400

    added speech functionality

--- script.js
+++ script.js
@@ -62,10 +62,21 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
-  // @todo - speak event
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  })
 
   main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
 
 // Store voices
 let voices = [];
@@ -82,6 +93,19 @@ function getVoices() {
     voicesSelect.appendChild(option);
   })
 }
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+
 
 // Voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices);
